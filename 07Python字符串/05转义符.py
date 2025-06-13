9261923c6a6486b483ac6392ade71fdcf01d7e73
maoyln <maoyl_yx@163.com>
2025-06-13 18:38:46 +0800

    字符串方法

---
+++ 07Python字符串/05转义符.py
@@ -0,0 +1,14 @@
+# Python字符串中转义符的使用方法
+# Python常用转义字符表
+
+print('---------黄金分割线-----------')
+
+# 使用换行符\n
+print("nihao\nshijie")
+# 输出：
+# nihao
+# shijie
+
+# 使用Tab制表符\t
+print("nihao\tshijie")
+# 输出: nihao    shijie
