302ba7ac3d764537949807f88eb4570c8b9ad425
maoyln <maoyl_yx@163.com>
2025-06-16 10:14:07 +0800

    python运算符

---
+++ 09Python运算符/02多行语句.py
@@ -0,0 +1,10 @@
+# 多行语句
+
+# Python 通常是一行写完一条语句，但如果语句很长，我们可以使用反斜杠 \ 来实现多行语句，例如：
+item_one = 1
+item_two = 2
+item_three = 3
+total = item_one + \
+        item_two + \
+        item_three
+print(total) # 输出为 6
