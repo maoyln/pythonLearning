b7dbfcb636244d6d0b9d593ea6c5e9cfa327b0c9
maoyln <maoyl_yx@163.com>
2025-06-19 18:06:11 +0800

    for in 循环

---
+++ 16Python循环/05for语句.py
@@ -0,0 +1,32 @@
+# for 语句
+# Python for 循环可以遍历任何可迭代对象，如一个列表或者一个字符串
+
+sites = ["Baidu", "Google","Runoob","Taobao"]
+for site in sites:
+    print(site)
+
+'''
+Baidu
+Google
+Runoob
+Taobao
+'''
+
+print('---------黄金分割线-----------')
+
+
+
+# 也可用于打印字符串中的每个字符：
+word = 'runoob'
+ 
+for letter in word:
+    print(letter)
+
+'''
+r
+u
+n
+o
+o
+b
+'''
