c03ff75833a5241e3c9b7271e27a64565627f792
maoyln <maoyl_yx@163.com>
2025-06-19 17:46:30 +0800

    猜字谜游戏

--- 15Python条件控制/02if语句.py
+++ 15Python条件控制/02if语句.py
@@ -16,3 +16,34 @@ print ("Good bye!")
 100
 Good bye!
 '''
+
+
+print('---------黄金分割线-----------')
+
+
+age = int(input("请输入你家狗狗的年龄: "))
+print("")
+if age <= 0:
+    print("你是在逗我吧!")
+elif age == 1:
+    print("相当于 14 岁的人。")
+elif age == 2:
+    print("相当于 22 岁的人。")
+elif age > 2:
+    human = 22 + (age -2)*5
+    print("对应人类年龄: ", human)
+ 
+### 退出提示
+input("点击 enter 键退出")
+
+
+print('---------黄金分割线-----------')
+
+
+# 程序演示了 == 操作符
+# 使用数字
+print(5 == 6)
+# 使用变量
+x = 5
+y = 8
+print(x == y)
