1f12aae355f81f2a701abd89ca41b92384ab007a
maoyln <maoyl_yx@163.com>
2025-06-19 18:03:07 +0800

    简单语句

---
+++ 16Python循环/04简单语句组.py
@@ -0,0 +1,20 @@
+# 简单语句组
+
+# 类似 if 语句的语法，如果你的 while 循环体中只有一条语句，你可以将该语句与 while 写在同一行中， 如下所示：
+
+flag = 1
+ 
+while (flag): print ('欢迎访问https://github.com/maoyln/pythonLearning!')
+ 
+print ("Good bye!")
+
+# 注意：以上的无限循环你可以使用 CTRL+C 来中断循环。
+
+'''
+欢迎访问https://github.com/maoyln/pythonLearning!
+欢迎访问https://github.com/maoyln/pythonLearning!
+欢迎访问https://github.com/maoyln/pythonLearning!
+欢迎访问https://github.com/maoyln/pythonLearning!
+欢迎访问https://github.com/maoyln/pythonLearning!
+……
+'''
