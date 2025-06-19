3d5d7f32004b9c6414b69678dc0dbc4e03031fcd
maoyln <maoyl_yx@163.com>
2025-06-19 17:37:23 +0800

    条件语句

---
+++ 15Python条件控制/02if语句.py
@@ -0,0 +1,18 @@
+# if 语句
+
+var1 = 100
+if var1:
+    print ("1 - if 表达式条件为 true")
+    print (var1)
+ 
+var2 = 0
+if var2:
+    print ("2 - if 表达式条件为 true")
+    print (var2)
+print ("Good bye!")
+
+'''
+1 - if 表达式条件为 true
+100
+Good bye!
+'''
